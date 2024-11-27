fc638c5d75428a6869a25900f38e3460c61e5751
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-11-27 23:23:28 +0700

    Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,11 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-      const success = true; // Simulate an API response condition
+      const success = true;
 
       if (success) {
-          resolve("API response received!");
+          resolve('API response received!');
       } else {
-          reject("Failed to fetch API response.");
+          reject(new Error('Failed to fetch API response.'));
       }
   });
 }
